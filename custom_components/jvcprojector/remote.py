db906b501731a52438de4863e5b394095073323a
Shahzeb Imran <s.imran@tuta.io>
2022-07-16 02:35:59 +1000

    improved exception handling, correct calling of sync from async

--- custom_components/jvcprojector/remote.py
+++ custom_components/jvcprojector/remote.py
@@ -3,34 +3,54 @@ from homeassistant.components import remote
 from homeassistant.const import DEVICE_DEFAULT_NAME
 from homeassistant import util
 import asyncio
+from typing import Final
 
-from homeassistant.const import (CONF_HOST, CONF_NAME, CONF_PASSWORD)
+JVC_RETRIES: Final = "max_retries"
+
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_PORT,
+    CONF_NAME,
+    CONF_PASSWORD,
+    CONF_TIMEOUT,
+    CONF_DELAY,
+)
+
 _LOGGER = logging.getLogger(__name__)
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the remote."""
     if config.get(CONF_HOST) is not None:
         host = config.get(CONF_HOST)
+        port = config.get(CONF_PORT)
         name = config.get(CONF_NAME)
         password = config.get(CONF_PASSWORD)
-    add_entities([
-        JVCRemote(name, host, password),
-    ])
+        timeout = config.get(CONF_TIMEOUT)
+        delay = config.get(CONF_DELAY)
+        retries = config.get(JVC_RETRIES)
+    add_entities(
+        [
+            JVCRemote(name, host, password, port, delay, timeout, retries),
+        ]
+    )
 
 
 class JVCRemote(remote.RemoteEntity):
     """Home assistant JVC remote representation"""
 
-    def __init__(self, name, host, password):
+    def __init__(self, name, host, password, port, delay, timeout, retries):
         """Initialize the Remote."""
         from jvc_projector import JVCProjector
+        from jvc_projector import JVCPoweredOffError
+
         self._name = name or DEVICE_DEFAULT_NAME
         self._host = host
         self._password = password
         self._last_command_sent = None
-        self._jvc = JVCProjector(host, password)
+        self._jvc = JVCProjector(host, password, port, delay, timeout, retries)
         self._state = None
-        self._power_state = 'N/A'
+        self._power_state = "N/A"
         self.state_lock = asyncio.Lock()
 
     @property
@@ -56,24 +76,26 @@ class JVCRemote(remote.RemoteEntity):
     def extra_state_attributes(self):
         """Return device state attributes."""
 
-        if self._power_state in ['lamp_on', 'reserved']:
+        if self._power_state in ["lamp_on", "reserved"]:
             self._state = True
         else:
             self._state = False
 
         return {
-            'last_command_sent': self._last_command_sent if self._last_command_sent is not None else 'N/A',
-            'power_state': self._power_state,
+            "last_command_sent": self._last_command_sent
+            if self._last_command_sent is not None
+            else "N/A",
+            "power_state": self._power_state,
         }
 
     async def async_turn_on(self, **kwargs):
         """Turn the remote on."""
-        await self.async_send_command('power_on')
+        await self.async_send_command("power-on")
         self._state = True
 
     async def async_turn_off(self, **kwargs):
         """Turn the remote off."""
-        await self.async_send_command('power_off')
+        await self.async_send_command("power-off")
         self._state = False
 
     async def async_send_command(self, command, **kwargs):
@@ -86,10 +108,16 @@ class JVCRemote(remote.RemoteEntity):
             for com in command:
                 _LOGGER.info(f"sending command: {com}")
                 try:
-                    command_sent = await hass.async_add_executor_job(self._jvc.command(com))
-                except Exception:
+                    command_sent = await self.hass.async_add_executor_job(
+                        self._jvc.command, (com)
+                    )
+                except JVCPoweredOffError as e:
+                    # The projector is powered off
+                    _LOGGER.error(f"Failed to send command, projector is powered off")
+                except Exception as e:
                     # when an error occured during sending, command execution probably failed
                     command_sent = False
+                    raise e
 
                 if not command_sent:
                     self._last_command_sent = "N/A"
@@ -98,13 +126,16 @@ class JVCRemote(remote.RemoteEntity):
                     self._last_command_sent = com
 
     async def async_update_state(self):
-        """"Update the state with the Power Status (if available)"""
+        """ "Update the state with the Power Status (if available)"""
 
         # do nothing until lock is released
         if self.state_lock.locked():
             return
 
         try:
-            self._power_state = await hass.async_add_executor_job(self._jvc.power_state())
-        except Exception:
-            self._power_state = 'unknown'
+            self._power_state = await self.hass.async_add_executor_job(
+                self._jvc.power_state
+            )
+        except Exception as e:
+            self._power_state = "unknown"
+            raise e
